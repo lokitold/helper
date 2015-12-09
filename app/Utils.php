b23e1c02bac9382c10e83eec8356de8e970dc644
victor <victor.rojas@ec.pe>
2015-12-09 12:33:07 -0500

    primercommit

---
+++ app/Utils.php
@@ -0,0 +1,19 @@
+<?php 
+
+namespace Lokitold\Helper\Utils;
+/**
+ * Lokitold Helper utils
+ * 
+ * @author     Victor Rojas <vico.16c@gmail.com>
+ * @version    0.1.0
+ * @package    Maer
+ * @subpackage Csrf
+ */
+class Utils 
+{
+     
+    public function Test()
+    {
+        return 'hello';
+    }
+}
